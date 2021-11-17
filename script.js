145f139e4a64e19f65b20abfc2c740a2cfce7f09
Tyler Moroz <moroztyler@gmail.com>
2021-11-16 19:32:06 -0700

    added functionality which prevented players from selecting tiles that were already selected

--- script.js
+++ script.js
@@ -6,14 +6,33 @@ const gameModule = (() => {
     })();
 
     const displayController = (() => {
+        let player1 = 'active';
+        let player2 = '';
         const gameCell = [...document.querySelectorAll('.gamecell')].forEach(function (cell){
             cell.addEventListener('click', function(){
-                const choice = document.createElement('p');
-                choice.textContent = 'x';
-                cell.appendChild(choice);  
-                Gameboard.gameBoard.push(choice.textContent);
-                console.log(choice.textContent);
-                console.log(Gameboard.gameBoard);
+
+                if(player1 == 'active' && cell.textContent == ''){
+                    cell.textContent = 'x';
+                    player2 = 'active';
+                    player1 = '';
+                } else if(player2 == 'active' && cell.textContent == ''){
+                    cell.textContent = 'o';
+                    player1 = 'active';
+                    player2 = '';
+                } else if(player2 == 'active' && cell.textContent == 'x'){
+                    cell.textContent = 'x';
+                    player2 = 'active';
+                    player1 = '';
+                    alert("Player 1 has already selected this tile.");
+                } else if(player1 == 'active' && cell.textContent == 'o'){
+                    cell.textContent = 'o';
+                    player1 = 'active';
+                    player2 = '';
+                    alert("Player 2 has already selected this tile.");
+                }
+ 
+                Gameboard.gameBoard.push(cell.textContent);
+                
             })
         });          
         return { gameCell, };
