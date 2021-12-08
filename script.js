5e80008fd996ff07e7f21a5628cc88abd246494f
Tyler Moroz <moroztyler@gmail.com>
2021-12-08 16:03:16 -0700

    fixed an issue where the game could continue  after a player got 3 in a row

--- script.js
+++ script.js
@@ -44,6 +44,14 @@ const gameModule = (() => {
 
 
     const displayController = (() => {
+        const row1 = [];
+        const row2 = [];
+        const row3 = [];
+        const column1 = [];
+        const column2 = [];
+        const column3 = [];
+        const diagonal1 = [];
+        const diagonal2 = [];
 
         const tieGame = arr => arr.includes('x', 'o');
 
@@ -55,107 +63,170 @@ const gameModule = (() => {
 
         playerOneActive.classList.add('active');
 
-        const gameCell = [...document.querySelectorAll('.gamecell')].forEach(function (cell){
-            cell.addEventListener('click', function(){
+        const gameLogic = function(){
 
-                if (player1 == 'active'){
-                    playerTwoActive.classList.add('active');
-                    playerOneActive.classList.remove('active');
-                } else if (player2 == 'active'){
-                    playerOneActive.classList.add('active');
-                    playerTwoActive.classList.remove('active');
-                }
-                
+            if(player1 == 'active' && this.textContent == ''){
+                this.textContent = 'x';
+                player2 = 'active';
+                player1 = '';
+                playerTwoActive.classList.add('active');
+                playerOneActive.classList.remove('active');
+            } else if(player2 == 'active' && this.textContent == ''){
+                this.textContent = 'o';
+                player1 = 'active';
+                player2 = '';
+                playerOneActive.classList.add('active');
+                playerTwoActive.classList.remove('active');
+            } else if(player2 == 'active' && this.textContent == 'x'){
+                player2 = 'active';
+                player1 = '';
+                playerTwoActive.classList.add('active');
+                playerOneActive.classList.remove('active');
+                alert("Player 1 has already selected this tile.");
+            } else if(player1 == 'active' && this.textContent == 'o'){
+                player1 = 'active';
+                player2 = '';
+                playerOneActive.classList.add('active');
+                playerTwoActive.classList.remove('active');
+                alert("Player 2 has already selected this tile.");
+            } else if (player2 == 'active' && this.textContent == 'o'){
+                player2 = 'active';
+                player1 = '';
+                playerTwoActive.classList.add('active');
+                playerOneActive.classList.remove('active');
+                alert("You have already selected this tile.");
+            } else if (player1 == 'active' && this.textContent == 'x'){
+                player1 = 'active';
+                player2 = '';
+                playerOneActive.classList.add('active');
+                playerTwoActive.classList.remove('active');
+                alert("You have already selected this tile.");
+            }
 
-                if(player1 == 'active' && cell.textContent == ''){
-                    cell.textContent = 'x';
-                    player2 = 'active';
-                    player1 = '';
-                } else if(player2 == 'active' && cell.textContent == ''){
-                    cell.textContent = 'o';
-                    player1 = 'active';
-                    player2 = '';
-                } else if(player2 == 'active' && cell.textContent == 'x'){
-                    cell.textContent = 'x';
-                    player2 = 'active';
-                    player1 = '';
-                    alert("Player 1 has already selected this tile.");
-                } else if(player1 == 'active' && cell.textContent == 'o'){
-                    cell.textContent = 'o';
-                    player1 = 'active';
-                    player2 = '';
-                    alert("Player 2 has already selected this tile.");
-                }
+            if(this == cell1){
+                Gameboard.gameBoard[0] = cell1.textContent;
+                row1.push(cell1.textContent);
+                column1.push(cell1.textContent);
+                diagonal1.push(cell1.textContent);
+            } else if(this == cell2){
+                Gameboard.gameBoard[1] = cell2.textContent;
+                row1.push(cell2.textContent);
+                column2.push(cell2.textContent);
+            } else if(this == cell3){
+                Gameboard.gameBoard[2] = cell3.textContent;
+                row1.push(cell3.textContent);
+                column3.push(cell3.textContent);
+                diagonal2.push(cell3.textContent);
+            } else if(this == cell4){
+                Gameboard.gameBoard[3] = cell4.textContent;
+                row2.push(cell4.textContent);
+                column1.push(cell4.textContent);
+            } else if(this == cell5){
+                Gameboard.gameBoard[4] = cell5.textContent;
+                row2.push(cell5.textContent);
+                column2.push(cell5.textContent);
+                diagonal1.push(cell5.textContent);
+                diagonal2.push(cell5.textContent);
+            } else if(this == cell6){
+                Gameboard.gameBoard[5] = cell6.textContent;
+                row2.push(cell6.textContent);
+                column3.push(cell6.textContent);
+            } else if(this == cell7){
+                Gameboard.gameBoard[6] = cell7.textContent;
+                row3.push(cell7.textContent);
+                column1.push(cell7.textContent);
+                diagonal2.push(cell7.textContent);
+            } else if(this == cell8){
+                Gameboard.gameBoard[7] = cell8.textContent;
+                row3.push(cell8.textContent);
+                column2.push(cell8.textContent);
+            } else if(this == cell9){
+                Gameboard.gameBoard[8] = cell9.textContent;
+                row3.push(cell9.textContent);
+                column3.push(cell9.textContent);
+                diagonal1.push(cell9.textContent);
+            }
 
-                if(cell == cell1){
-                    Gameboard.gameBoard[0] = cell1.textContent;
-                } else if(cell == cell2){
-                    Gameboard.gameBoard[1] = cell2.textContent;
-                } else if(cell == cell3){
-                    Gameboard.gameBoard[2] = cell3.textContent;
-                } else if(cell == cell4){
-                    Gameboard.gameBoard[3] = cell4.textContent;
-                } else if(cell == cell5){
-                    Gameboard.gameBoard[4] = cell5.textContent;
-                } else if(cell == cell6){
-                    Gameboard.gameBoard[5] = cell6.textContent;
-                } else if(cell == cell7){
-                    Gameboard.gameBoard[6] = cell7.textContent;
-                } else if(cell == cell8){
-                    Gameboard.gameBoard[7] = cell8.textContent;
-                } else if(cell == cell9){
-                    Gameboard.gameBoard[8] = cell9.textContent;
-                }
+            console.log(Gameboard.gameBoard);
 
-                console.log(Gameboard.gameBoard);
+            const endGame = function(){
+                cell1.removeEventListener('click', gameLogic);
+                cell2.removeEventListener('click', gameLogic);
+                cell3.removeEventListener('click', gameLogic);
+                cell4.removeEventListener('click', gameLogic);
+                cell5.removeEventListener('click', gameLogic);
+                cell6.removeEventListener('click', gameLogic);
+                cell7.removeEventListener('click', gameLogic);
+                cell8.removeEventListener('click', gameLogic);
+                cell9.removeEventListener('click', gameLogic);
+            }
 
-                if(Gameboard.gameBoard[0] =='x' && Gameboard.gameBoard[1] == 'x' && Gameboard.gameBoard[2] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[3] == 'x' && Gameboard.gameBoard[4] =='x' && Gameboard.gameBoard[5] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[6] =='x' && Gameboard.gameBoard[7] =='x' && Gameboard.gameBoard[8] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[0] =='x' && Gameboard.gameBoard[3] =='x' && Gameboard.gameBoard[6] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[1] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[7] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[2] == 'x' && Gameboard.gameBoard[5] == 'x' && Gameboard.gameBoard[8] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[0] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[8] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[2] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[6] == 'x'){
-                    console.log('player 1 wins');
-                } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[1] == 'o' && Gameboard.gameBoard[2] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[3] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[5] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[6] == 'o' && Gameboard.gameBoard[7] == 'o' && Gameboard.gameBoard[8] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[3] == 'o' && Gameboard.gameBoard[6] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[1] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[7] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[2] == 'o' && Gameboard.gameBoard[5] == 'o' && Gameboard.gameBoard[8] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[8] == 'o'){
-                    console.log('player 2 wins');
-                } else if(Gameboard.gameBoard[2] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[6] == 'o'){
-                    console.log('player 2 wins');
-                }
-            })
-        });
+            if(Gameboard.gameBoard[0] =='x' && Gameboard.gameBoard[1] == 'x' && Gameboard.gameBoard[2] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[3] == 'x' && Gameboard.gameBoard[4] =='x' && Gameboard.gameBoard[5] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[6] =='x' && Gameboard.gameBoard[7] =='x' && Gameboard.gameBoard[8] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[0] =='x' && Gameboard.gameBoard[3] =='x' && Gameboard.gameBoard[6] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[1] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[7] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[2] == 'x' && Gameboard.gameBoard[5] == 'x' && Gameboard.gameBoard[8] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[0] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[8] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[2] == 'x' && Gameboard.gameBoard[4] == 'x' && Gameboard.gameBoard[6] == 'x'){
+                console.log('player 1 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[1] == 'o' && Gameboard.gameBoard[2] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[3] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[5] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[6] == 'o' && Gameboard.gameBoard[7] == 'o' && Gameboard.gameBoard[8] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[3] == 'o' && Gameboard.gameBoard[6] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[1] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[7] == 'o'){
+                console.log('player 2 wins');
+            } else if(Gameboard.gameBoard[2] == 'o' && Gameboard.gameBoard[5] == 'o' && Gameboard.gameBoard[8] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[0] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[8] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            } else if(Gameboard.gameBoard[2] == 'o' && Gameboard.gameBoard[4] == 'o' && Gameboard.gameBoard[6] == 'o'){
+                console.log('player 2 wins');
+                endGame();
+            }
 
+            if(row1.length == 3 && row2.length == 3 && row3.length == 3 && column1.length == 3 && column2.length == 3 && column3.length == 3 && diagonal1.length == 3 && diagonal2.length == 3 && tieGame(row1) && tieGame(row2) && tieGame(row3) && tieGame(column1) && tieGame(column2) && tieGame(column3) && tieGame(diagonal1) && tieGame(diagonal2)){
+                console.log("Tie Game!");
+            }
+            
+        }
 
-        // const tie = document.getElementById('gameboard');
-
-        // tie.addEventListener('click', function(){
-        //     if (row1Array.length == 3 && row2Array.length == 3 && row3Array.length == 3 && column1Array.length == 3 && column2Array.length == 3 && column3Array.length == 3 && diagonal1Array.length == 3 && diagonal2Array.length == 3 && tieGame(row1Array) && tieGame(row2Array) && tieGame(row3Array) && tieGame(column1Array) && tieGame(column2Array) && tieGame(column3Array) && tieGame(diagonal1Array) && tieGame(diagonal2Array)){
-        //         console.log("Tie Game");
-        //     }
-        // });
-
+        cell1.addEventListener('click', gameLogic);
+        cell2.addEventListener('click', gameLogic);
+        cell3.addEventListener('click', gameLogic);
+        cell4.addEventListener('click', gameLogic);
+        cell5.addEventListener('click', gameLogic);
+        cell6.addEventListener('click', gameLogic);
+        cell7.addEventListener('click', gameLogic);
+        cell8.addEventListener('click', gameLogic);
+        cell9.addEventListener('click', gameLogic);
         
-        return { gameCell, };
+        return { };
     })();
 
     return { Gameboard,
